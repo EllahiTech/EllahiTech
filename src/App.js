31e9a6cb20c9eb42abd65be38d927a3416ba3ffd
EllahiTech <saad.s1709@gmail.com>
2024-05-25 19:13:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import {Home} from './pages/home';
+import {Home} from './home';
 
 import { WhatsAppChatButton } from './components/WhatsAppChatButton';
 import { ScrollToTopButton } from './components/ScrollToTopButton';
